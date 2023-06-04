f5cdf4909fc5dba8316b720f269697bd49b9170c
Dima-1990 <99199420+Dima-1990@users.noreply.github.com>
2023-06-04 17:01:26 +0300

    task 2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+ 
 
-console.log(galleryItems);
+
+const myGalleryUlEl = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(
+    ({ original, preview, description }) => `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>`
+  )
+    .join('');
+  
+ 
+
+
+ myGalleryUlEl.innerHTML = galleryMarkup;
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  sourceAttr: 'href',
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionsData: 'alt',
+});
+
+
+
+
+// console.log(galleryItems);
