88d227faf4ba50387d324984ef31ad77e5b367af
Dima-1990 <99199420+Dima-1990@users.noreply.github.com>
2023-06-04 22:56:08 +0300

    tasc 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,6 +14,6 @@ const getCurrentTime = function (currentTime) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
 };
 
-player.on('timeupdate', throttle(getCurrentTime, 5000));
+player.on('timeupdate', throttle(getCurrentTime, 3000));
 
-player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')));
+player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,64 @@
+import throttle from 'lodash.throttle';
 
+
+
+
+
+const myFormEl = document.querySelector('.feedback-form');
+const formFirstLabel = myFormEl.firstElementChild;
+const formSecondLabel = formFirstLabel.nextElementSibling;
+const btnSubmitEl = myFormEl.lastElementChild;
+
+let enteredValues = {};
+
+defoltValueInput();
+
+
+myFormEl.addEventListener('input',throttle((e) => {
+
+  if (e.target.type === formFirstLabel.lastElementChild.name) {
+    enteredValues.email = e.target.value;
+  } else {
+    enteredValues.message = e.target.value;
+  }
+
+  
+    pushLocalStorage(enteredValues);
+
+},500));
+
+function pushLocalStorage(object) {
+localStorage.setItem('feedback-form-state', JSON.stringify(object));
+}
+
+function defoltValueInput() {
+    
+    let infoFromLocal = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    if (infoFromLocal !== null) {
+        formFirstLabel.lastElementChild.value = infoFromLocal.email;
+        formSecondLabel.lastElementChild.value = infoFromLocal.message;
+    }
+}
+
+btnSubmitEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    let resultValue = {
+      email: formFirstLabel.lastElementChild.value,
+      message: formSecondLabel.lastElementChild.value,
+    };
+
+if (
+  formFirstLabel.lastElementChild.value === '' ||
+  formSecondLabel.lastElementChild.value === ''
+) {
+    alert('Не всі поля заповнені');
+    return
+}
+
+
+    console.log(resultValue);
+
+    myFormEl.reset();
+    localStorage.clear();
+});
