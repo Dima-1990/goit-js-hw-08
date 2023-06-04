b38ea22d784d1ea203560fd6a1ceb446748c8d36
Dima-1990 <99199420+Dima-1990@users.noreply.github.com>
2023-06-04 23:11:10 +0300

    а

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,5 @@
+
 import throttle from 'lodash.throttle';
-
-
-
-
 
 const myFormEl = document.querySelector('.feedback-form');
 const formFirstLabel = myFormEl.firstElementChild;
@@ -13,52 +10,208 @@ let enteredValues = {};
 
 defoltValueInput();
 
+myFormEl.addEventListener(
+  'input',
+  throttle(e => {
+    if (e.target.type === formFirstLabel.lastElementChild.name) {
+      enteredValues.email = e.target.value;
+    } else {
+      enteredValues.message = e.target.value;
+    }
 
-myFormEl.addEventListener('input',throttle((e) => {
-
-  if (e.target.type === formFirstLabel.lastElementChild.name) {
-    enteredValues.email = e.target.value;
-  } else {
-    enteredValues.message = e.target.value;
-  }
-
-  
     pushLocalStorage(enteredValues);
-
-},500));
+  }, 500)
+);
 
 function pushLocalStorage(object) {
-localStorage.setItem('feedback-form-state', JSON.stringify(object));
+  localStorage.setItem('feedback-form-state', JSON.stringify(object));
 }
 
 function defoltValueInput() {
-    
+  try {
     let infoFromLocal = JSON.parse(localStorage.getItem('feedback-form-state'));
 
     if (infoFromLocal !== null) {
-        formFirstLabel.lastElementChild.value = infoFromLocal.email;
-        formSecondLabel.lastElementChild.value = infoFromLocal.message;
+      formFirstLabel.lastElementChild.value = infoFromLocal.email;
+      formSecondLabel.lastElementChild.value = infoFromLocal.message;
     }
+  } catch (error) {
+    console.log('помилка поля вводу');
+  }
 }
 
-btnSubmitEl.addEventListener('click', (e) => {
-    e.preventDefault();
-    let resultValue = {
-      email: formFirstLabel.lastElementChild.value,
-      message: formSecondLabel.lastElementChild.value,
-    };
+btnSubmitEl.addEventListener('click', e => {
+  e.preventDefault();
+  let resultValue = {
+    email: formFirstLabel.lastElementChild.value,
+    message: formSecondLabel.lastElementChild.value,
+  };
 
-if (
-  formFirstLabel.lastElementChild.value === '' ||
-  formSecondLabel.lastElementChild.value === ''
-) {
+  if (
+    formFirstLabel.lastElementChild.value === '' ||
+    formSecondLabel.lastElementChild.value === ''
+  ) {
     alert('Не всі поля заповнені');
-    return
-}
+    return;
+  }
 
+  console.log(resultValue);
 
-    console.log(resultValue);
-
-    myFormEl.reset();
-    localStorage.clear();
+  myFormEl.reset();
+  localStorage.clear();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import throttle from 'lodash.throttle';
+
+
+
+
+
+// const myFormEl = document.querySelector('.feedback-form');
+// const formFirstLabel = myFormEl.firstElementChild;
+// const formSecondLabel = formFirstLabel.nextElementSibling;
+// const btnSubmitEl = myFormEl.lastElementChild;
+
+// let enteredValues = {};
+
+// defoltValueInput();
+
+
+// myFormEl.addEventListener('input',throttle((e) => {
+
+//   if (e.target.type === formFirstLabel.lastElementChild.name) {
+//     enteredValues.email = e.target.value;
+//   } else {
+//     enteredValues.message = e.target.value;
+//   }
+
+  
+//     pushLocalStorage(enteredValues);
+
+// },500));
+
+// function pushLocalStorage(object) {
+// localStorage.setItem('feedback-form-state', JSON.stringify(object));
+// }
+
+// function defoltValueInput() {
+    
+//     let infoFromLocal = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+//     if (infoFromLocal !== null) {
+//         formFirstLabel.lastElementChild.value = infoFromLocal.email;
+//         formSecondLabel.lastElementChild.value = infoFromLocal.message;
+//     }
+// }
+
+// btnSubmitEl.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     let resultValue = {
+//       email: formFirstLabel.lastElementChild.value,
+//       message: formSecondLabel.lastElementChild.value,
+//     };
+
+// if (
+//   formFirstLabel.lastElementChild.value === '' ||
+//   formSecondLabel.lastElementChild.value === ''
+// ) {
+//     alert('Не всі поля заповнені');
+//     return
+// }
+
+
+//     console.log(resultValue);
+
+//     myFormEl.reset();
+//     localStorage.clear();
+// });
